90a0f485bb93a4e838d911c607d5dfe0cf91e619
Jenita Lusiana Lukmawati <jenitalusii@gmail.com>
2023-09-05 19:57:36 +0700

    bebas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,19 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function profile($nama = "", $kelas = "", $npm = ""){
+        //ketika menggunakan array, untuk mengassign ke suatu nilai ke index itu pake => 
+        //index (key) itu yg disebelah kiri panah 'nama'
+        //value itu yg disebelah kanan panah
+        //dipisahkan oleh tanda koma (,)
+        $data = [
+            //key     //value
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'npm' => $npm
+        ];
+
+        return view('profile', $data);
+    }
 }
