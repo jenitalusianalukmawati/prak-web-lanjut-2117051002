8c5b6b269c1a8944a8a3b3450e310c347e19b1a0
Jenita Lusiana Lukmawati <jenitalusii@gmail.com>
2023-09-09 00:01:13 +0700

    bebass

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -4,24 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <?php echo '<link rel="stylesheet" href="./style.css">'; ?>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><?= $nama ?></td>
-        </tr>
-        <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td><?= $kelas ?></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><?= $npm ?></td>
-        </tr>
-    </table>
+    <div class="profile-area">
+        <div class="container">
+            <div class="row">
+                <div class="main">
+                    <div class="card">
+                        <div class="img1"><img src="<?=base_url('./img/bg.jpg')?>"></div>
+                        <div class="img2"><img src="<?=base_url('./img/jen.png')?>"></div>
+                        <div class="text">
+                            <h2>Jenita Lusiana Lukmawati</h2>
+                            <p>B</p>
+                            <p>2117051002</p>
+                        </div>
+                        <div class="sosmed">
+                            <a href="https://twitter.com"><i class="fa fa-twitter"></i></a>
+                            <a href="https://instagram.com"><i class="fa fa-instagram"></i></a>
+                            <a href="https://linkedin.com"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
