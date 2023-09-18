11c37edc9bed356dfed1037f0d46e01569e2cfff
Jenita Lusiana Lukmawati <jenitalusii@gmail.com>
2023-09-18 18:56:08 +0700

    pesan commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes ->get('profile', 'Home::profile');
+$routes->get('profile', 'Home::profile');
 $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class, 'create']);
 $routes->post('/user/store', [UserController::class, 'store']);
--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <link rel="stylesheet" href="<?=base_url("assets/css/styleform.css")?>">
+    <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
 </head>
 <body>
     <section class="wrapper">
