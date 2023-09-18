0920c2cde342fc043c471b38a440ba372d2bda1e
Jenita Lusiana Lukmawati <jenitalusii@gmail.com>
2023-09-18 17:42:13 +0700

    formnew

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Controllers\UserController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes ->get('profile', 'Home::profile');
+$routes->get('/user/profile', [UserController::class, 'profile']);
+$routes->get('/user/create', [UserController::class, 'create']);
+$routes->post('/user/store', [UserController::class, 'store']);
