7c8c52f4e9bda1e08bc6ba062cdd245a97418378
Jenita Lusiana Lukmawati <jenitalusii@gmail.com>
2023-09-26 07:48:55 +0700

    db-model

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\KelasModel;
+use App\Models\UserModel;
 
 class UserController extends BaseController{
     public function index(){
@@ -19,16 +21,73 @@ class UserController extends BaseController{
     }
 
     public function create(){
-        return view('create_user');
+         $kelas = [
+            [
+                'id' => 1,
+                'nama_kelas' => 'A'
+            ],
+            [
+                'id' => 2,
+                'nama_kelas' => 'B'
+            ],
+            [
+                'id' => 3,
+                'nama_kelas' => 'C'
+            ],
+            [
+                'id' => 4,
+                'nama_kelas' => 'D'
+            ],
+         ];
+
+         if (session('validation') != null) {
+            $validation = session('validation');
+        } else {
+            $validation = \Config\Services::validation();
+        }
+
+         $data = [
+            'kelas' => $kelas,
+            'validation' => $validation,
+         ];
+
+        return view('create_user', $data);
     }
 
     public function store(){
         //dd($this->request->getVar());
-        $data = [
-            'nama' => $this->request->getVar('nama'),
-            'kelas' => $this->request->getVar('kelas'),
-            'npm' => $this->request->getVar('npm'),
+
+        // validasi input
+        if(!$this->validate([
+            'nama' => 'required',
+            'npm' => 'required|is_unique[user.npm]',
+        ])){
+            $validation = \Config\Services::validation();
+            return redirect()->to('/user/create')->withInput()->with('validation', $validation);
+            
+            //dd($validation); 
+            // return redirect()->to(base_url('/user/create'));
+        } 
+
+        $userModel = new UserModel();
+        $kelasModel = new KelasModel();
+        $nama = $this->request->getPost('nama');
+        $npm = $this->request->getPost('npm');
+        $kelas = $this->request->getPost('kelas');
+
+        $data=[
+            'nama' => $nama,
+            'npm' => $npm,
+            'id_kelas' => $kelas
         ];
-        return view('profile', $data);
+
+        $userModel->saveUser($data);
+        $data_new=[
+            'nama' => $nama,
+            'npm' => $npm,
+            'id_kelas' => $kelasModel->find($kelas)['nama_kelas']
+        ];
+
+        return view('profile', $data_new);
     }
 }
--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
 </head>
@@ -12,9 +13,26 @@
         <div class="form">
             <header>FORM</header>
             <form action="<?= base_url('/user/store') ?>" method="POST">
-                <input type="text" name="nama" placeholder="Nama" required/>
-                <input type="text" name="kelas" placeholder="Kelas" required/>
-                <input type="text" name="npm" placeholder="NPM" required/>
+                <input type="text" name="nama" placeholder="Nama" class="form-control <?= ($validation->hasError('nama')) ?
+                'is-invalid' : ''; ?>" id="nama" autofocus>
+                <div class="invalid-feedback">                    <?= $validation->getError('nama') ?>
+                </div>
+                <select name="kelas" id="kelas">
+                    <?php
+                    foreach ($kelas as $item){
+                    ?>
+                        <option value="<?= $item['id'] ?>">
+                            <?= $item['nama_kelas'] ?>
+                        </option>
+                    <?php
+                    }
+                    ?>
+                </select>
+                <input type="text" name="npm" placeholder="NPM" class="form-control <?= ($validation->hasError('npm')) ?
+                'is-invalid' : ''; ?>" id="npm" autofocus>
+                <div class="invalid-feedback">
+                    <?= $validation->getError('npm') ?>
+                </div>
                 <input type="submit" name="submit" value="Send" />
             </form>
         </div>
